e57afae6d74adf32c916ee3fb610c2b47489c5ba
Ckala <agr.akyla@mail.ru>
2024-12-09 11:10:43 +0300

    ( feature ) added method in WarehouseService find part with color for another year.

--- backend/app/Services/WarehouseService.php
+++ backend/app/Services/WarehouseService.php
@@ -77,7 +77,36 @@ class WarehouseService implements WarehouseServiceInterface
 
         $sql = SqlScripts::getSqlQueryWithColor();
 
+        $data = DB::connection("dax")->select($sql, [
+            "number" => $this->getCurrentYear($code)
+        ]);
+
+        dd($data);
+
+        $timeExecute = TimerExecuteService::Stop();
+
+        //партия может быть не найдена!
+        if (!$data) {
+            return [];
+        }
+
+        dd($data);
+    }
+
+    /**
+     * Get party with color and user '/^([0-9]{1,2}[%]{1}[0-9]{4,10})[@]$/'
+     * @param string $code
+     * @return array
+     */
+    public function executeCommandFindCellWithColorAndUserAnotherYear(string $code): array
+    {
+        TimerExecuteService::Start();
+
+        $sql = SqlScripts::getSqlQueryWithColor();
+
         $data = DB::connection("dax")->select($sql, ["number" => $code]);
+
+        $timeExecute = TimerExecuteService::Stop();
 
         //партия может быть не найдена!
         if (!$data) {
